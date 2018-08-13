66be16c54a76d64cfa28870ea38eb55425260e23
mike castleman <m@mlcastle.net>
2018-08-13 02:52:56 -0400

    html futzing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 cp --content-type 'text/html;charset=utf-8' --cache-control 'public,max-age=86400' index.html s3://words.oulipo.link/index.html
+aws s3 cp --content-type 'text/html;charset=utf-8' --content-language 'en-US' --cache-control 'public,max-age=86400' index.html s3://words.oulipo.link/index.html
