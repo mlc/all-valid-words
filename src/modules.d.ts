c9a49b1f1a1dba22e5895a4f2f62f22be423143c
mike castleman <m@mlcastle.net>
2019-07-14 19:25:08 -0400

    taking gzip typings from npm

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,20 +1,5 @@
 /* eslint-disable import/export */
 
-declare module 'node-gzip' {
-  import { ZlibOptions } from 'zlib';
-
-  type GzipInput = Buffer | NodeJS.TypedArray | DataView | ArrayBuffer | string;
-
-  export function gzip(
-    input: GzipInput,
-    options?: ZlibOptions
-  ): Promise<Buffer>;
-  export function ungzip(
-    input: GzipInput,
-    options?: ZlibOptions
-  ): Promise<Buffer>;
-}
-
 declare module 'random-number-csprng' {
   export default function secureRandomNumber(
     minimum: number,
