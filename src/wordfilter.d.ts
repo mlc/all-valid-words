0dc36c8c31e4fee10ac500c00e51fc13ee33a6d7
mike castleman <m@mlcastle.net>
2021-02-08 14:14:47 -0500

    wordlist typings from npm

--- src/wordfilter.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'wordfilter' {
-  export function blacklisted(string: string): boolean;
-  export function addWords(array: string | ReadonlyArray<string>): void;
-  export function removeWord(word: string): void;
-  export function clearList(): void;
-}
